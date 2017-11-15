d9b7e723e97a9d0415b395636f948b81dc141aed
kant97 <kirant9797@gmail.com>
2017-11-15 12:28:33 +0300

    add parsing without brackets after function/procedure

--- paskal_function_header/parser.cpp
+++ paskal_function_header/parser.cpp
@@ -5,7 +5,8 @@
 #include "parser.h"
 #include "parse_exception.h"
 
-std::shared_ptr<parser::node> parser::S()  {
+std::shared_ptr<parser::node> parser::S()
+{
     node res("S");
     std::shared_ptr<node> ch1;
     std::shared_ptr<node> ch2;
@@ -16,28 +17,18 @@ std::shared_ptr<parser::node> parser::S()  {
     std::shared_ptr<node> ch7;
     auto ch8 = std::make_shared<node>(";");
     switch (lexer_ptr->get_cur_token()) {
-        case token::FUNCTION :
-            ch1 = std::make_shared<node>("function");
-            res.add_children(ch1);
-            lexer_ptr->next_token();
+    case token::FUNCTION :
+        ch1 = std::make_shared<node>("function");
+        res.add_children(ch1);
+        lexer_ptr->next_token();
 
-            parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::NAME);
-            ch2 = std::make_shared<node>(lexer_ptr->get_cur_attribute());
-            res.add_children(ch2);
-            lexer_ptr->next_token();
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::NAME);
+        ch2 = std::make_shared<node>(lexer_ptr->get_cur_attribute());
+        res.add_children(ch2);
+        lexer_ptr->next_token();
 
-            parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::LBR);
-            res.add_children(ch3);
-            lexer_ptr->next_token();
-
-            ch4 = A();
-            res.add_children(ch4);
-
-            parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::RBR);
-            res.add_children(ch5);
-            lexer_ptr->next_token();
-
-            parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::DOTDOT);
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::LBR || lexer_ptr->get_cur_token() == token::DOTDOT);
+        if (lexer_ptr->get_cur_token() == token::DOTDOT) {
             ch6 = std::make_shared<node>(":");
             res.add_children(ch6);
             lexer_ptr->next_token();
@@ -53,169 +44,211 @@ std::shared_ptr<parser::node> parser::S()  {
 
             parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::END);
             break;
-        case token::PROCEDURE :
-            ch1 = std::make_shared<node>("procedure");
-            res.add_children(ch1);
-            lexer_ptr->next_token();
+        }
+        res.add_children(ch3);
+        lexer_ptr->next_token();
 
-            parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::NAME);
-            ch2 = std::make_shared<node>(lexer_ptr->get_cur_attribute());
-            res.add_children(ch2);
-            lexer_ptr->next_token();
+        ch4 = A();
+        res.add_children(ch4);
 
-            parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::LBR);
-            res.add_children(ch3);
-            lexer_ptr->next_token();
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::RBR);
+        res.add_children(ch5);
+        lexer_ptr->next_token();
 
-            ch4 = A();
-            res.add_children(ch4);
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::DOTDOT);
+        ch6 = std::make_shared<node>(":");
+        res.add_children(ch6);
+        lexer_ptr->next_token();
 
-            parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::RBR);
-            res.add_children(ch5);
-            lexer_ptr->next_token();
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::NAME);
+        ch7 = std::make_shared<node>(lexer_ptr->get_cur_attribute());
+        res.add_children(ch7);
+        lexer_ptr->next_token();
 
-            parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::DOTCOMA);
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::DOTCOMA);
+        res.add_children(ch8);
+        lexer_ptr->next_token();
+
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::END);
+        break;
+    case token::PROCEDURE :
+        ch1 = std::make_shared<node>("procedure");
+        res.add_children(ch1);
+        lexer_ptr->next_token();
+
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::NAME);
+        ch2 = std::make_shared<node>(lexer_ptr->get_cur_attribute());
+        res.add_children(ch2);
+        lexer_ptr->next_token();
+
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::LBR || lexer_ptr->get_cur_token() == token::DOTCOMA);
+        if (lexer_ptr->get_cur_token() == token::DOTCOMA) {
             ch6 = std::make_shared<node>(";");
             res.add_children(ch6);
             lexer_ptr->next_token();
 
             parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::END);
             break;
-        default:
-            throw parse_exception("Unknown function/procedure declaration");
+        }
+        res.add_children(ch3);
+        lexer_ptr->next_token();
+
+        ch4 = A();
+        res.add_children(ch4);
+
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::RBR);
+        res.add_children(ch5);
+        lexer_ptr->next_token();
+
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::DOTCOMA);
+        ch6 = std::make_shared<node>(";");
+        res.add_children(ch6);
+        lexer_ptr->next_token();
+
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::END);
+        break;
+    default:
+        throw parse_exception("Unknown function/procedure declaration");
     }
     return std::make_shared<node>(res);
 }
 
-std::shared_ptr<parser::node> parser::A()  {
+std::shared_ptr<parser::node> parser::A()
+{
     node res("A");
     std::shared_ptr<node> ch;
     std::shared_ptr<node> ch1;
     switch (lexer_ptr->get_cur_token()) {
-        case NAME :
-            ch = B();
-            res.add_children(ch);
+    case NAME :
+        ch = B();
+        res.add_children(ch);
 
-            ch1 = APrime();
-            res.add_children(ch1);
-            break;
-        case RBR:
-            ch = std::make_shared<node>(")");
-            break;
-        default:
-            throw parse_exception("Expected argument name, found smth other");
+        ch1 = APrime();
+        res.add_children(ch1);
+        break;
+    case RBR:
+        ch = std::make_shared<node>(")");
+        break;
+    default:
+        throw parse_exception("Expected argument name, found smth other");
     }
     return std::make_shared<node>(res);
 }
 
-std::shared_ptr<parser::node> parser::APrime()  {
+std::shared_ptr<parser::node> parser::APrime()
+{
     node res("A'");
     auto ch = std::make_shared<node>(";");
     std::shared_ptr<node> ch1;
     std::shared_ptr<node> ch3;
-    switch(lexer_ptr->get_cur_token()) {
-        case token::DOTCOMA:
-            res.add_children(ch);
-            lexer_ptr->next_token();
+    switch (lexer_ptr->get_cur_token()) {
+    case token::DOTCOMA:
+        res.add_children(ch);
+        lexer_ptr->next_token();
 
-            ch1 = B();
-            res.add_children(ch1);
+        ch1 = B();
+        res.add_children(ch1);
 
-            ch3 = APrime();
-            res.add_children(ch3);
-            break;
-        case token::RBR:
-            break;
-        default:
-            throw parse_exception("; expected");
+        ch3 = APrime();
+        res.add_children(ch3);
+        break;
+    case token::RBR:
+        break;
+    default:
+        throw parse_exception("; expected");
     }
     return std::make_shared<node>(res);
 }
 
-parser::iterator parser::parse(std::string file_name)  {
+parser::iterator parser::parse(std::string file_name)
+{
     lexer_ptr = std::make_shared<lexical_analyzer>(std::move(file_name));
     lexer_ptr->next_token();
     return parser::iterator(parser::S());
 }
 
-std::string parser::iterator::get_name() const {
+std::string parser::iterator::get_name() const
+{
     return v->state_name;
 }
 
-std::vector<parser::iterator> parser::iterator::get_all_children() const {
+std::vector<parser::iterator> parser::iterator::get_all_children() const
+{
     std::vector<iterator> ans;
-    for (auto ch: v->children) {
+    for (auto ch : v->children)
         ans.push_back(iterator(ch));
-    }
     return ans;
 }
 
-std::shared_ptr<parser::node> parser::B()  {
+std::shared_ptr<parser::node> parser::B()
+{
     node res("B");
     std::shared_ptr<node> ch;
     auto ch1 = std::make_shared<node>(":");
     std::shared_ptr<node> ch2;
-    switch(lexer_ptr->get_cur_token()) {
-        case token::NAME:
-            ch = C();
-            res.add_children(ch);
+    switch (lexer_ptr->get_cur_token()) {
+    case token::NAME:
+        ch = C();
+        res.add_children(ch);
 
-            parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::DOTDOT);
-            res.add_children(ch1);
-            lexer_ptr->next_token();
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::DOTDOT);
+        res.add_children(ch1);
+        lexer_ptr->next_token();
 
-            parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::NAME);
-            ch2 = std::make_shared<node>(lexer_ptr->get_cur_attribute());
-            res.add_children(ch2);
-            lexer_ptr->next_token();
-            break;
-        default:
-            throw parse_exception("expected function argument, found smth else");
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::NAME);
+        ch2 = std::make_shared<node>(lexer_ptr->get_cur_attribute());
+        res.add_children(ch2);
+        lexer_ptr->next_token();
+        break;
+    default:
+        throw parse_exception("expected function argument, found smth else");
     }
     return std::make_shared<node>(res);
 }
 
-std::shared_ptr<parser::node> parser::C()  {
+std::shared_ptr<parser::node> parser::C()
+{
     node res("C");
     std::shared_ptr<node> ch;
     std::shared_ptr<node> ch1;
-    switch(lexer_ptr->get_cur_token()) {
-        case token::NAME :
-            ch = std::make_shared<node>(lexer_ptr->get_cur_attribute());
-            res.add_children(ch);
-            lexer_ptr->next_token();
+    switch (lexer_ptr->get_cur_token()) {
+    case token::NAME :
+        ch = std::make_shared<node>(lexer_ptr->get_cur_attribute());
+        res.add_children(ch);
+        lexer_ptr->next_token();
 
-            ch1 = D();
-            res.add_children(ch1);
-            break;
-        default:
-            throw parse_exception("expected function argument, found smth else");
+        ch1 = D();
+        res.add_children(ch1);
+        break;
+    default:
+        throw parse_exception("expected function argument, found smth else");
     }
     return std::make_shared<node>(res);
 }
 
-std::shared_ptr<parser::node> parser::D()  {
+std::shared_ptr<parser::node> parser::D()
+{
     node res("D");
     auto ch = std::make_shared<node>(",");
     std::shared_ptr<node> ch1;
     std::shared_ptr<node> ch3;
-    switch(lexer_ptr->get_cur_token()) {
-        case token::COMA:
-            res.add_children(ch);
-            lexer_ptr->next_token();
+    switch (lexer_ptr->get_cur_token()) {
+    case token::COMA:
+        res.add_children(ch);
+        lexer_ptr->next_token();
 
-            parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::NAME);
-            ch1 = std::make_shared<node>(lexer_ptr->get_cur_attribute());
-            res.add_children(ch1);
-            lexer_ptr->next_token();
+        parse_exception::parse_assert(lexer_ptr->get_cur_token() == token::NAME);
+        ch1 = std::make_shared<node>(lexer_ptr->get_cur_attribute());
+        res.add_children(ch1);
+        lexer_ptr->next_token();
 
-            ch3 = D();
-            res.add_children(ch3);
-            break;
-        case token::DOTDOT:
-            break;
-        default:
-            throw parse_exception("smth went wrong");
+        ch3 = D();
+        res.add_children(ch3);
+        break;
+    case token::DOTDOT:
+        break;
+    default:
+        throw parse_exception("smth went wrong");
     }
     return std::make_shared<node>(res);
 }
